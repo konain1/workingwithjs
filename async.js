08d67da771d15308e24fad6592c6eea2c476a705
konain1 <kaunainkasab@gmail.com>
2023-09-04 16:04:29 +0530

    loginAsyncAwait

--- async.js
+++ async.js
@@ -1,15 +1,36 @@
 
 
-const pokemon = async ()=>{
+// const pokemon = async ()=>{
 
     
 
-    throw  'wait..............'
-    return 'Pika Pika Pikachu'
+//     throw  'wait..............'
+//     return 'Pika Pika Pikachu'
+// }
+
+// pokemon().then((data)=>{
+//     console.log('i choose you ' +data)
+// }).catch((err)=>{
+//     throw err
+// })
+
+
+const login = async (username,password) =>{
+    if(!username || !password){
+        throw 'Missing Crediential !'
+    }
+
+    if(password === 'doora'){
+        return 'welcome'
+    }
+
+    throw 'wrong password'
+
 }
 
-pokemon().then((data)=>{
-    console.log('i choose you ' +data)
-}).catch((err)=>{
-    throw err
+
+login('zidane','doora').then((data)=>{
+    console.log(data)
+}).catch((data)=>{
+    console.log(data)
 })
