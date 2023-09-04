2e626fe2106ee117ff17cd55907b92d3608ca413
konain1 <kaunainkasab@gmail.com>
2023-09-04 16:22:39 +0530

    rainbow

--- async.js
+++ async.js
@@ -15,22 +15,52 @@
 // })
 
 
-const login = async (username,password) =>{
-    if(!username || !password){
-        throw 'Missing Crediential !'
-    }
+// const login = async (username,password) =>{
+//     if(!username || !password){
+//         throw 'Missing Crediential !'
+//     }
 
-    if(password === 'doora'){
-        return 'welcome'
-    }
+//     if(password === 'doora'){
+//         return 'welcome'
+//     }
 
-    throw 'wrong password'
+//     throw 'wrong password'
 
+// }
+
+
+// login('zidane','doora').then((data)=>{
+//     console.log(data)
+// }).catch((data)=>{
+//     console.log(data)
+// })
+
+const changeColorPromise = (color,delay)=>{
+    return new Promise((resolve,reject)=>{
+
+        setTimeout(() => {
+            
+            document.body.style.backgroundColor = color;
+            resolve('background has changed');
+        }, delay);
+
+
+    })
 }
 
 
-login('zidane','doora').then((data)=>{
-    console.log(data)
-}).catch((data)=>{
-    console.log(data)
-})
+async function rainbow(){
+    await changeColorPromise('red',1000)
+    await changeColorPromise('green',1000)
+    await changeColorPromise('yellow',1000)
+    await changeColorPromise('magenta',1000)
+    await changeColorPromise('lightgreen',1000)
+    await changeColorPromise('pink',1000)
+
+
+
+
+
+}
+
+rainbow()
